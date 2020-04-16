dfd9895f329bb788b1b401427b459c069894e692
yenvy0512 <kieuyenvy0512@gmail.com>
2020-04-16 22:20:42 +0700

    2 files modified, 12 added, 8 deleted

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,9 +15,11 @@ import { ManageComponent } from './backend/manage/manage.component';
 import { ProductEditComponent } from './backend/product-edit/product-edit.component';
 import { ProductDetailComponent } from './frontend/product-detail/product-detail.component';
 import { ProductListComponent } from './frontend/product-list/product-list.component';
+import { DashboardComponent } from './backend/dashboard/dashboard.component';
+import { AdminComponent } from './backend/admin/admin.component';
 @NgModule({
   imports:      [ BrowserModule,AppRoutingModule, FormsModule,AngularFontAwesomeModule,HttpClientModule ],
-  declarations: [ AppComponent, HelloComponent, HeaderComponent, FooterComponent, HomeComponent, ProductAddComponent, ManageComponent, ProductEditComponent, ProductDetailComponent, ProductListComponent ],
+  declarations: [ AppComponent, HelloComponent, HeaderComponent, FooterComponent, HomeComponent, ProductAddComponent, ManageComponent, ProductEditComponent, ProductDetailComponent, ProductListComponent, DashboardComponent, AdminComponent ],
   bootstrap:    [ AppComponent ],
   providers: [ProductService]
 })
