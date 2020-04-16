afd7f2ce387e64e396f7d3f9b4e35c6c93c9187d
yenvy0512 <kieuyenvy0512@gmail.com>
2020-04-17 00:47:54 +0700

    3 files modified, 1 added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,15 +6,24 @@ import {ProductAddComponent } from './backend/product-add/product-add.component'
 import { ManageComponent } from './backend/manage/manage.component';
 import { ProductEditComponent } from './backend/product-edit/product-edit.component';
 import { ProductDetailComponent } from './frontend/product-detail/product-detail.component';
+import { DashboardComponent } from './backend/dashboard/dashboard.component';
+import { AdminComponent } from './backend/admin/admin.component';
 const routes: Routes = [
    {path: 'home',component: HomeComponent},
  { path: '', redirectTo: '/home', pathMatch: 'full' },
- {path: 'manage',component: ManageComponent},
+ 
   {path: 'product',component: ManageComponent},
   {path: 'product-list',component: ProductListComponent},
   {path: 'product-add',component: ProductAddComponent},
       {path: 'product/edit/:productID', component: ProductEditComponent},
-         {path: 'product/:id', component: ProductDetailComponent}
+         {path: 'product/:id', component: ProductDetailComponent},
+           {path: 'admin',component: AdminComponent,
+children:[
+  {path: '',redirectTo: 'dashboard',pathMatch: 'full'},
+  {path: 'dashboard',component: DashboardComponent},
+   {path: 'manage',component: ManageComponent}
+]
+  }
 ];
 
 @NgModule({
